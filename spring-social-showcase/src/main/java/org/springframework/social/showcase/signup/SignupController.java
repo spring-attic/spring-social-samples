a4db01a901c297f67bf9332276da31576ec74249
Craig Walls <cwalls@vmware.com>
2011-02-23 17:51:45 -0600

    Moved connect-after-signup functionality out of ConnectController

--- spring-social-showcase/src/main/java/org/springframework/social/showcase/signup/SignupController.java
+++ spring-social-showcase/src/main/java/org/springframework/social/showcase/signup/SignupController.java
@@ -15,28 +15,45 @@
  */
 package org.springframework.social.showcase.signup;
 
+import java.io.Serializable;
+
 import javax.inject.Inject;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.social.showcase.ShowcaseUser;
 import org.springframework.social.showcase.UserRepository;
 import org.springframework.social.showcase.UsernameAlreadyInUseException;
+import org.springframework.social.web.connect.ProviderSignInAccount;
+import org.springframework.social.web.connect.ServiceProviderLocator;
 import org.springframework.social.web.connect.SignInService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
-public class SignupController {
+public class SignupController implements BeanFactoryAware {
 
 	private final UserRepository userRepository;
+
 	private final SignInService signinService;
+
+	private ServiceProviderLocator serviceProviderLocator;
 
 	@Inject
 	public SignupController(UserRepository userRepository, SignInService signinService) {
 		this.userRepository = userRepository;
 		this.signinService = signinService;
+	}
+
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		serviceProviderLocator = new ServiceProviderLocator((ListableBeanFactory) beanFactory);
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
@@ -45,11 +62,17 @@ public class SignupController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@Valid SignupForm form, BindingResult formBinding) {
+	public String signup(@Valid SignupForm form, BindingResult formBinding, WebRequest request) {
 		if (formBinding.hasErrors()) {
 			return null;
 		}
-		return createUser(form, formBinding) ? "redirect:/" : null;
+		boolean userCreated = createUser(form, formBinding);
+		if (userCreated) {
+			createConnection(request, form.getUsername());
+			return "redirect:/";
+		}
+
+		return null;
 	}
 
 	private boolean createUser(SignupForm form, BindingResult formBinding) {
@@ -65,4 +88,13 @@ public class SignupController {
 			return false;
 		}
 	}
+
+	private void createConnection(WebRequest request, Serializable accountId) {
+		ProviderSignInAccount signInAccount = (ProviderSignInAccount) request.getAttribute("providerSignInAccount", WebRequest.SCOPE_SESSION);
+		if(signInAccount != null) {
+			request.removeAttribute("providerSignInAccount", WebRequest.SCOPE_SESSION);
+			signInAccount.connect(serviceProviderLocator, accountId);
+		}
+	}
+
 }
