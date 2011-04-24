c643f266044b246d127dfd9b2292c99c90656aa9
Keith Donald <keith.donald@springsource.com>
2011-04-24 13:17:05 -0400

    fixed exception if not signed in

---
+++ spring-social-showcase/src/main/java/org/springframework/social/showcase/ConnectedToHandlerInterceptor.java
@@ -0,0 +1,27 @@
+package org.springframework.social.showcase;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.social.connect.ServiceProviderConnectionRepository;
+import org.springframework.social.facebook.api.FacebookApi;
+import org.springframework.social.twitter.api.TwitterApi;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class ConnectedToHandlerInterceptor extends HandlerInterceptorAdapter {
+
+	@Inject
+	private ServiceProviderConnectionRepository connectionRepository;
+		
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (request.getUserPrincipal() != null) {
+			request.setAttribute("connectedToTwitter", connectionRepository.findPrimaryConnectionToServiceApi(TwitterApi.class) != null);
+			request.setAttribute("connectedToFacebook", connectionRepository.findPrimaryConnectionToServiceApi(FacebookApi.class) != null);
+		}
+		return true;
+	}
+
+
+}
