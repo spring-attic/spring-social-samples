08879925f7d6029ceccfb11d48100472efc1ec8e
Keith Donald <keith.donald@springsource.com>
2011-06-11 00:50:35 -0400

    polish

--- spring-social-quickstart/src/main/java/org/springframework/social/quickstart/HomeController.java
+++ spring-social-quickstart/src/main/java/org/springframework/social/quickstart/HomeController.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.social.quickstart.home;
+package org.springframework.social.quickstart;
 
 import java.util.List;
 
