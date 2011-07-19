656f636a4a28ccc9e6ffa121c742cb579cf92fec
Craig Walls <cwalls@vmware.com>
2011-07-19 10:27:01 -0500

    SimpleSignInAdapter polish

--- spring-social-showcase/src/main/java/org/springframework/social/showcase/signin/SimpleSignInAdapter.java
+++ spring-social-showcase/src/main/java/org/springframework/social/showcase/signin/SimpleSignInAdapter.java
@@ -17,7 +17,10 @@ package org.springframework.social.showcase.signin;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.social.connect.Connection;
@@ -36,13 +39,26 @@ public class SimpleSignInAdapter implements SignInAdapter {
 	@Override
 	public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
 		SignInUtils.signin(localUserId);
-	    HttpServletRequest httpServletRequest = (HttpServletRequest) request.getNativeRequest();
-		SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, null); 
-		requestCache.removeRequest(httpServletRequest, null);
-	    if (savedRequest != null) { 
-	        return savedRequest.getRedirectUrl(); 
-	    }	    
-	    return null; 
+		return extractOriginalUrl(request);
+	}
+
+	private String extractOriginalUrl(NativeWebRequest request) {
+		HttpServletRequest nativeReq = request.getNativeRequest(HttpServletRequest.class);
+		HttpServletResponse nativeRes = request.getNativeResponse(HttpServletResponse.class);
+		SavedRequest saved = requestCache.getRequest(nativeReq, nativeRes);
+		if (saved == null) {
+			return null;
+		}
+		requestCache.removeRequest(nativeReq, nativeRes);
+		removeAutheticationAttributes(nativeReq.getSession(false));
+		return saved.getRedirectUrl();
+	}
+		 
+	private void removeAutheticationAttributes(HttpSession session) {
+		if (session == null) {
+			return;
+		}
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
 	}
 
 }
