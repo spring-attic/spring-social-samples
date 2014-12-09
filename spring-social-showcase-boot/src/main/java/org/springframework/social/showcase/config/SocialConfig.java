be4c48e18a905249d9cd253e9558fa6746a5e7e2
Craig Walls <cwalls@pivotal.io>
2014-12-09 14:23:15 -0600

    Sync up

--- spring-social-showcase-boot/src/main/java/org/springframework/social/showcase/config/SocialConfig.java
+++ spring-social-showcase-boot/src/main/java/org/springframework/social/showcase/config/SocialConfig.java
@@ -4,17 +4,26 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.github.connect.GitHubConnectionFactory;
 
 @Configuration
 public class SocialConfig extends SocialConfigurerAdapter {
 
 	@Inject
 	private DataSource dataSource;
+	
+	@Override
+	public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment env) {
+		connectionFactoryConfigurer.addConnectionFactory(
+				new GitHubConnectionFactory(env.getProperty("spring.social.github.appId"), env.getProperty("spring.social.github.appSecret")));
+	}
 	
 	/**
 	 * Singleton data access object providing access to connections across all users.
