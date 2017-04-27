391528761bcef52c9bd0884c96b09b5e3804506b
Craig Walls <cwalls@pivotal.io>
2017-04-27 11:49:59 -0600

    Build against Spring Boot 2.0.0.BUILD-SNAPSHOT and Spring 5.0.0.BUILD-SNAPSHOT

--- spring-social-showcase/src/main/java/org/springframework/social/showcase/config/Application.java
+++ spring-social-showcase/src/main/java/org/springframework/social/showcase/config/Application.java
@@ -1,13 +1,23 @@
 package org.springframework.social.showcase.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.social.showcase.signin.SimpleSignInAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.dialect.SpringStandardDialect;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @ComponentScan(basePackages="org.springframework.social.showcase")
 @EnableConfigurationProperties
@@ -20,6 +30,34 @@ public class Application {
 	@Bean
 	public SignInAdapter signInAdapter() {
 		return new SimpleSignInAdapter(new HttpSessionRequestCache());
+	}
+	
+	@Bean
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+		thymeleafViewResolver.setTemplateEngine(templateEngine());
+		return thymeleafViewResolver;
+	}
+	
+	@Bean
+	public ITemplateResolver templateResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setPrefix("templates/");
+		templateResolver.setSuffix(".html");
+		return templateResolver;
+	}
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setMessageSource(messageSource);
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.addDialect(new LayoutDialect());
+		templateEngine.addDialect(new SpringSecurityDialect());
+		return templateEngine;
 	}
 
 }
