0e87d82c20ca293d3a379d3bca3721c4f2847ba4
Mehmet Melik Kose <mekose@opendns.com>
2017-12-16 14:39:52 +0000

    twitter secretary initial code.

---
+++ spring-social-showcase/src/main/resources/db/migration/V20171215231350955__db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE account (
+  `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT
+  COMMENT 'Unique identifier for Account',
+  `username`  VARCHAR(40) UNIQUE,
+  `password`  VARCHAR(60) NOT NULL,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname`  VARCHAR(60) NOT NULL,
+  CONSTRAINT pk_account PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 COMMENT='Stores Service and Component Mapping Data.';
+
+insert into account (username, password, firstname, lastname) values ('habuma', 'tacos', 'Craig', 'Walls');
+insert into account (username, password, firstname, lastname) values ('rclarkson', 'atlanta', 'Roy', 'Clarkson');
+
